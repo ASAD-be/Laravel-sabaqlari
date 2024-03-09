3cc246edf2d881cff4fc326c0ec68e7426885173
ASAD-be <156946347+ASAD-be@users.noreply.github.com>
2024-03-09 22:20:33 +0500

    asdf
    asdf

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -7,15 +7,12 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+{ 
     public function run()
     {
         Role::create(['name' => 'manager']);
         Role::create(['name' => 'client']);
+      
+        
     }
 }
